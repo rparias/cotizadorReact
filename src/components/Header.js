422060a9ac3036a80c3f87b00c57c5395bd477e3
Ronald Arias <ronald.arias@thoughtworks.com>
2020-03-19 15:46:31 -0500

    Added @emotion/core y @emotion/styled libraries

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import styled from '@emotion/styled';
+
+const ContainerHeader = styled.header`
+  background-color: #26c6da;
+  padding: 10px;
+  font-weight: bold;
+  color: #ffffff;
+`;
+
+const TextHeader = styled.h1`
+  font-size: 2rem;
+  margin: 0;
+  font-family: 'Slabo 27px', serif;
+  text-align: center;
+`;
 
 function Header({ title }) {
   return (
-    <header>
-      <h1>{title}</h1>
-    </header>
+    <ContainerHeader>
+      <TextHeader>{title}</TextHeader>
+    </ContainerHeader>
   );
 }
 
